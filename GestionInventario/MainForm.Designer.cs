e33d2c4b827a0cdce4b21e777d3683acfbf30dd3
Andres Cubillos <andrestj1996@gmail.com>
2023-04-27 13:08:01 -0400

    Interfaz Basica

--- GestionInventario/MainForm.Designer.cs
+++ GestionInventario/MainForm.Designer.cs
@@ -30,25 +30,96 @@ namespace GestionInventario
         private void InitializeComponent()
         {
             this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainTab = new System.Windows.Forms.TabControl();
+            this.pageVentas = new System.Windows.Forms.TabPage();
+            this.pageProductos = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.MainMenu.SuspendLayout();
+            this.MainTab.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainMenu
             // 
+            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
             this.MainMenu.Location = new System.Drawing.Point(0, 0);
             this.MainMenu.Name = "MainMenu";
             this.MainMenu.Size = new System.Drawing.Size(800, 24);
             this.MainMenu.TabIndex = 0;
             this.MainMenu.Text = "MainMenu";
             // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // MainTab
+            // 
+            this.MainTab.Controls.Add(this.pageVentas);
+            this.MainTab.Controls.Add(this.pageProductos);
+            this.MainTab.Controls.Add(this.tabPage3);
+            this.MainTab.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainTab.Location = new System.Drawing.Point(0, 24);
+            this.MainTab.Name = "MainTab";
+            this.MainTab.SelectedIndex = 0;
+            this.MainTab.Size = new System.Drawing.Size(800, 426);
+            this.MainTab.TabIndex = 1;
+            // 
+            // pageVentas
+            // 
+            this.pageVentas.Location = new System.Drawing.Point(4, 22);
+            this.pageVentas.Name = "pageVentas";
+            this.pageVentas.Padding = new System.Windows.Forms.Padding(3);
+            this.pageVentas.Size = new System.Drawing.Size(792, 400);
+            this.pageVentas.TabIndex = 0;
+            this.pageVentas.Text = "Ventas";
+            this.pageVentas.UseVisualStyleBackColor = true;
+            // 
+            // pageProductos
+            // 
+            this.pageProductos.Location = new System.Drawing.Point(4, 22);
+            this.pageProductos.Name = "pageProductos";
+            this.pageProductos.Padding = new System.Windows.Forms.Padding(3);
+            this.pageProductos.Size = new System.Drawing.Size(792, 400);
+            this.pageProductos.TabIndex = 1;
+            this.pageProductos.Text = "Productos";
+            this.pageProductos.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(792, 400);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Clientes";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MainTab);
             this.Controls.Add(this.MainMenu);
             this.MainMenuStrip = this.MainMenu;
             this.Name = "MainForm";
             this.Text = "Gestion Inventario";
+            this.MainMenu.ResumeLayout(false);
+            this.MainMenu.PerformLayout();
+            this.MainTab.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +128,12 @@ namespace GestionInventario
         #endregion
 
         private System.Windows.Forms.MenuStrip MainMenu;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.TabControl MainTab;
+        private System.Windows.Forms.TabPage pageVentas;
+        private System.Windows.Forms.TabPage pageProductos;
+        private System.Windows.Forms.TabPage tabPage3;
     }
 }
 
