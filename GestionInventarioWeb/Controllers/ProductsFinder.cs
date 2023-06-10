a78a4b7a3203812cc2e6b3132e321e03520909ee
Andres <a.cubillos7@alumnos.santotomas.cl>
2023-06-09 22:30:16 -0400

    Agregue paginas de plantilla de productos y ventas. Ajustes en los estilos generales y login.

---
+++ GestionInventarioWeb/Controllers/ProductsFinder.cs
@@ -0,0 +1,33 @@
+﻿using GestionInventarioWeb.Data;
+using GestionInventarioWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionInventarioWeb.Controllers
+{
+    public class ProductsFinder
+    {
+        private readonly GestionInventarioContext _context;
+
+        public ProductsFinder(GestionInventarioContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> FindAllAsync()
+        {
+            var products = new List<Product>();
+
+            foreach(var p in await _context.Productos.Include(p => p.IdCategoriaNavigation).ToListAsync())
+            {
+                var s = await _context.Inventarios.OrderByDescending(i => i.Fecha).FirstOrDefaultAsync(i => i.IdProducto == p.Id);
+                var stock = 0;
+                if(s != null) {
+                    stock = s.Cantidad;
+                }
+                products.Add(new Product(p.Id, p.Ean, p.Nombre, p.Descripcion, p.Precio, p.IdCategoriaNavigation.Categoria1, stock));
+            }
+
+            return products;
+        }
+    }
+}
---
+++ GestionInventarioWeb/Data/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace GestionInventarioWeb.Data
+{
+    public class Product
+    {
+        public int Id { get; }
+        public string EAN { get; }
+        public string Name { get; }
+        public string Description { get; }
+        public int Price { get; }
+        public string Category { get; }
+        public int Stock { get; }
+
+        public Product(int id, string ean, string name, string description, int price, string category, int stock)
+        {
+            Id = id;
+            EAN = ean;
+            Name = name;
+            Description = description;
+            Price = price;
+            Category = category;
+            Stock = stock;
+        }
+    }
+}
